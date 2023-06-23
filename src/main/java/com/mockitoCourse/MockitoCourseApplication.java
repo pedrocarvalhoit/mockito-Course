8efa665e36ff3c0fd7a44b4646286f22e5ce9789
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-06-23 20:44:51 +0100

    Initial Commit

---
+++ src/main/java/com/mockitoCourse/MockitoCourseApplication.java
@@ -0,0 +1,13 @@
+package com.mockitoCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockitoCourse/MockitoCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockitoCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
