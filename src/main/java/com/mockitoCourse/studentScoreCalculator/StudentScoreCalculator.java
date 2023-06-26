0ab5dd6acf2d2926906aafc878b48ae1e160bb91
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-06-26 22:01:42 +0100

    Student Calculator

---
+++ src/main/java/com/mockitoCourse/studentScoreCalculator/StudentScoreCalculator.java
@@ -0,0 +1,19 @@
+package com.mockitoCourse.studentScoreCalculator;
+
+public class StudentScoreCalculator {
+
+    private int satScore;
+
+    public void calculateSATScore(int mathScore, int literacyScore){
+        if(mathScore < 0 || mathScore > 100 || literacyScore < 0 || literacyScore > 100){
+            satScore = -1;
+        }else{
+            satScore = mathScore * literacyScore;
+        }
+    }
+
+    public int getSatScore(){
+        return this.satScore;
+    }
+
+}
