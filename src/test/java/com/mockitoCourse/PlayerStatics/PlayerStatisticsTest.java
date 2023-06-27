567bb4c18f6df1797d7244610834c72a6108edcb
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-06-27 21:24:00 +0100

    Player Statistics Test

---
+++ src/test/java/com/mockitoCourse/PlayerStatics/PlayerStatisticsTest.java
@@ -0,0 +1,71 @@
+package com.mockitoCourse.PlayerStatics;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerStatisticsTest {
+
+    @Test
+    public void playerNamesEqual(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        Player p2 = new Player("Pedro Carvalho", 26);
+        //EQUALS just check de Name
+        assertEquals(p1, p2);
+    }
+
+    @Test
+    public void playerNamesNotEqual(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        Player p2 = new Player("Victor Duarte", 26);
+        assertNotEquals(p1, p2);
+    }
+
+    @Test
+    public void yougerPlayerName(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        Player p2 = new Player("Victor Duarte", 26);
+        assertSame(p2, PlayerStatistics.getYougerPlayer(p1,p2));
+    }
+
+    @Test
+    public void underThirtyTrue(){
+        PlayerStatistics statistics = new PlayerStatistics();
+        Player p1 = new Player("Pedro Carvalho", 30);
+        Player p2 = new Player("Victor Duarte", 26);
+        assertTrue(statistics.underThirty(p2));
+    }
+
+    @Test
+    public void underThirtyFalse(){
+        PlayerStatistics statistics = new PlayerStatistics();
+        Player p1 = new Player("Pedro Carvalho", 30);
+        Player p2 = new Player("Victor Duarte", 26);
+        assertFalse(statistics.underThirty(p1));
+    }
+
+    @Test
+    public void csvReportNull(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        PlayerStatistics statistics = new PlayerStatistics(p1, 0, 0);
+        assertNull(statistics.creatCsvRecord());
+    }
+
+    @Test
+    public void csvReportNotNull(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        PlayerStatistics statistics = new PlayerStatistics(p1, 3, 0);
+        assertNotNull(statistics.creatCsvRecord());
+    }
+
+    @Test
+    public void getCsvStatusRecord(){
+        Player p1 = new Player("Pedro Carvalho", 30);
+        PlayerStatistics statistics = new PlayerStatistics(p1, 4, 8);
+        Double[] expectedArray = {2d,0.5};
+        assertArrayEquals(expectedArray, statistics.creatCsvRecord());
+    }
+
+}
