b9260d4b96d02ab6c55d0f7328b769284ebdd4b1
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-07-11 20:49:52 +0100

    Goal Statistics Test

--- src/test/java/com/mockitoCourse/goalsStatistics/GoalStatisticsTest.java
+++ src/test/java/com/mockitoCourse/goalsStatistics/GoalStatisticsTest.java
@@ -1,12 +1,17 @@
 package com.mockitoCourse.goalsStatistics;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class GoalStatisticsTest {
 
     @Mock
@@ -19,10 +24,15 @@ class GoalStatisticsTest {
     public void testGoalsPerGame() throws Exception {
         String player = "Pedro Carvalho";
 
-        when(goalsService.getGoals(player)).thenReturn(100);
-        Integer goalsAvarage = goalStatistics.goalPerGame(player, 10);
-        assertEquals(10, goalsAvarage);
+        //First time will return 100 than second time 10
+        when(goalsService.getGoals(player)).thenReturn(100).thenReturn(10);
+        Integer goalsHigh = goalStatistics.goalPerGame(player, 10);
+        assertEquals(10, goalsHigh);
 
+        Integer goalsLow = goalStatistics.goalPerGame(player, 10);
+        assertEquals(1, goalsLow);
     }
+
+
 
 }
